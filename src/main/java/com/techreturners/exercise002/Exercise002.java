ed8101cceb844cf036aa4f36867aa31be60f6c41
Ben Donaghy <bendonaghy321@gmail.com>
2021-12-06 23:14:44 +0000

    created person class, uncommented Exercise002 tests, updated Exercise002, tests passing

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,22 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
+    public boolean isFromManchester(Person person) {
+        
+        if (person.getCity() == "Manchester") {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        
+        if (person.getAge() >= ageLimit) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
 }
