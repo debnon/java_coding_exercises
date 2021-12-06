e99233325ffabd52b2b8f81d3109a6ea396c4d19
Ben Donaghy <bendonaghy321@gmail.com>
2021-12-06 22:40:35 +0000

    updating Exercise001, all tests passing

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,44 @@
 package com.techreturners.exercise001;
 
+
 import java.util.List;
+import java.text.DecimalFormat;
+
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        
+        return word.substring(0,1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        
+        return firstName.charAt(0) + "." + lastName.charAt(0);
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+
+        originalPrice += originalPrice * vatRate / 100;
+        DecimalFormat priceFormat = new DecimalFormat("#.##");
+        double newPrice = Double.parseDouble(priceFormat.format(originalPrice));
+
+        return newPrice;
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        
+        return new StringBuffer(sentence).reverse().toString();
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+
+        int count = 0;
+        for (User user : users) {
+            if (user.getType() == "Linux") {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
