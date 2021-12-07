5a5cb20326e0b13258ab8456f9350ff9107a152e
Ben Donaghy <bendonaghy321@gmail.com>
2021-12-07 01:45:33 +0000

    added additional unit tests to Exercise003, and edited Exercise003 so tests are passing

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,41 @@
 package com.techreturners.exercise003;
 
+import java.util.ArrayList;
+
 public class Exercise003 {
 
+
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        // String [] iceCreamList;
+        // iceCreamList = iceCreamFlavours();
+        // for (String flavour : iceCreamList) {
+        //     if (flavour == "Chocolate") {
+        //         return 0;
+        //     } else if (flavour == "Chocolate") {
+
+        //     }
+        // }
+        String flavour = iceCreamFlavour;
+        if (flavour == "Pistachio") {
+            return 0;
+        } else if (flavour == "Raspberry Ripple") {
+            return 1;
+        } else if (flavour == "Vanilla") {
+            return 2;
+        } else if (flavour == "Mint Chocolate Chip") {
+            return 3;
+        } else if (flavour == "Chocolate") {
+            return 4;
+        } else {
+            return 5;
+        }
+        
+        // throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        String [] iceCreamList = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
+        return iceCreamList;
     }
 
 }
