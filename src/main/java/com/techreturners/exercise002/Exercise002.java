cce33efc59064a8618179987e6e3214be1334233
Ben Donaghy <bendonaghy321@gmail.com>
2021-12-10 16:05:50 +0000

    removed excess boolean statements and changed equality operator to equals method

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -3,20 +3,11 @@ package com.techreturners.exercise002;
 public class Exercise002 {
 
     public boolean isFromManchester(Person person) {
-        
-        if (person.getCity() == "Manchester") {
-            return true;
-        } else {
-            return false;
-        }
+        return (person.getCity().equals("Manchester"));
     }
 
     public boolean canWatchFilm(Person person, int ageLimit) {
         
-        if (person.getAge() >= ageLimit) {
-            return true;
-        } else {
-            return false;
-        }
+        return (person.getAge() >= ageLimit);
     }
 }
