2876cf74292eb2c257861f13bab3967a3cd30307
Ben Donaghy <bendonaghy321@gmail.com>
2021-12-11 17:49:46 +0000

    removed superfluous brackets from return

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -3,11 +3,12 @@ package com.techreturners.exercise002;
 public class Exercise002 {
 
     public boolean isFromManchester(Person person) {
-        return (person.getCity().equals("Manchester"));
+        
+        return person.getCity().equals("Manchester");
     }
 
     public boolean canWatchFilm(Person person, int ageLimit) {
         
-        return (person.getAge() >= ageLimit);
+        return person.getAge() >= ageLimit;
     }
 }
